83b1e3f7f2dfd2771b5da98224e993c74bd20ee4
Calvin <calvinchui382025@yahoo.com>
2023-02-13 15:55:09 -0600

    layout modification, navbar overhaul

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,49 +8,20 @@ import Image from 'next/image'
 const Root = styled('div')({
   display: 'flex',
   flexDirection: 'column',
-  width: '100%',
-  height: '100%',
+  // width: '100vw',
+  height: '100vh',
   backgroundImages: 'url(/backgroundimage2.jpeg)',
   // minHeight: '100vh',
 })
 
 //======================================================
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html>
       <head />
-      <body>
+      <body style={{ margin: 0 }}>
         <Root>
           <NavBar />
-          {/* <nav>
-            <Link href='/'>
-              Home
-            </Link>
-            <Link href='/who'>
-              Who We Are
-            </Link>
-            <Link href='/what'>
-              What We Do
-            </Link>
-            <Link href='/proficiency'>
-              Proficiency Requirements
-            </Link>
-            <Link href='/gear'>
-              Gear Requirements
-            </Link>
-          </nav> */}
-          <div style={{position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh'}}>
-            <Image
-            src={'/backgroundimage2.jpeg'}
-            alt='image'
-            quality={100}
-            fill
-            />
-          </div>
           {children}
         </Root>
       </body>
