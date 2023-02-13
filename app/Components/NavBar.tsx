9cd35fa0fd8c91dcab1f9d002485e0bf5d8892e2
Calvin <calvinchui382025@yahoo.com>
2023-02-13 13:27:09 -0600

    initial navbar set up

---
+++ app/Components/NavBar.tsx
@@ -0,0 +1,133 @@
+"use client"
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Link from 'next/link';
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+const drawerWidth = 240;
+const navItems = ['home', 'who', 'what', 'proficiency', 'gear'];
+
+export default function DrawerAppBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+    //======================================================
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        TDSC
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => {
+          let to = `/${item.toLowerCase()}`;
+          if (item === 'home') to = '/'
+          return (
+            <ListItem key={item} disablePadding>
+              <ListItemButton 
+                component={Link}
+                href={to}
+                // to={to}
+                // onClick={() => setSelectedPage(to)}
+                sx={{ textAlign: 'center' }}
+                >
+                <ListItemText primary={item} />
+              </ListItemButton>
+              {/* <Link href={to}>
+                {item}
+              </Link> */}
+            </ListItem>
+          )
+          }
+        )}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Texas Defensive Shooting Club
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => {
+              let to = `/${item.toLowerCase()}`;
+              if (item === 'home') to = '/'
+              return (
+                <Button 
+                  key={item} 
+                  sx={{ color: '#fff' }}
+                  component={Link}
+                  href={to}
+                >
+                  {item}
+                </Button>
+              )
+              })}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
