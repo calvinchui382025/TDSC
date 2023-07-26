39d33b31010e6f1cc51fa1a1bca9b559848162eb
Lucas Overbey <lucasaoverbey@gmail.com>
2023-07-26 15:25:26 -0500

    added styles to admin textfields

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import react, { useState } from "react"
-import { AdminContent, AdminRoot, AdminNavBar, AdminLoginWrapper } from "./AdminStyles"
+import { AdminContent, AdminRoot, AdminNavBar, AdminLoginWrapper, LoginTitle, CustomTextField } from "./AdminStyles"
 import TableRowsIcon from '@mui/icons-material/TableRows';
 import EmailIcon from '@mui/icons-material/Email';
 import { Button, Checkbox, Divider, TextField } from "@mui/material";
+import { createTheme, ThemeProvider, Theme, useTheme } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -15,6 +16,44 @@ import Paper from '@mui/material/Paper';
 import { greyColorCustom, greyColorCustomDark, greyColorCustomLight } from "app/utils";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const customTheme = (outerTheme: Theme) =>
+  createTheme({
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            '--TextField-brandBorderColor': 'rgba(51, 51, 51, 0.85)',
+            '--TextField-brandBorderHoverColor': '#B2BAC2',
+            '--TextField-brandBorderFocusedColor': '#6F7E8C',
+            '& label.Mui-focused': {
+              color: 'var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+      MuiFilledInput: {
+        styleOverrides: {
+          root: {
+            '&:before, &:after': {
+              borderBottom: '2px solid var(--TextField-brandBorderColor)',
+            },
+            '&:hover:not(.Mui-disabled, .Mui-error):before': {
+              borderBottom: '2px solid var(--TextField-brandBorderHoverColor)',
+            },
+            '&.Mui-focused:after': {
+              borderBottom: '2px solid var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+    },
+  });
 
 import EmailBlastModal from './email/EmailBlastModal'
 
@@ -29,6 +68,7 @@ function createData(
 }
 
 export default function JoinPage() {
+  const outerTheme = useTheme();
   const [adminLoggedIn, setAdminLoggedIn] = useState(false);
   const [adminUserName, setAdminUserName] = useState('');
   const [adminPassword, setAdminPassword] = useState('');
@@ -43,10 +83,23 @@ export default function JoinPage() {
     if (adminUserName === 'admin' && adminPassword === 'admin') {
       setAdminLoggedIn(true);
     }
+    else {
+      toast.warn('Invalid username or password', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+    }
   }
 
   return (
     <AdminRoot>
+      <ToastContainer />
       { adminLoggedIn ?
       <>
         <AdminNavBar>
@@ -59,22 +112,26 @@ export default function JoinPage() {
         <EmailBlastModal isOpen={isEmailOpen} closeFunc={handleCloseEmail} />
       </> : 
       <AdminLoginWrapper elevation={5}>
-        <h1>Admin Login</h1>
-        <TextField
-          label='Username'
-          variant='outlined'
-          value={adminUserName}
-          onChange={(e) => setAdminUserName(e.target.value)}
-        />
-        <TextField
-          label='Password'
-          variant='outlined'
-          value={adminPassword}
-          onChange={(e) => setAdminPassword(e.target.value)}
-        />
-        <Button variant='contained' onClick={handleAdminLogin}>
-          Login
-        </Button>
+        <ThemeProvider theme={customTheme(outerTheme)}>
+          <LoginTitle>Admin Login</LoginTitle>
+          <CustomTextField
+            margin="dense"
+            label='Username'
+            variant='filled'
+            value={adminUserName}
+            onChange={(e) => setAdminUserName(e.target.value)}
+          />
+          <CustomTextField
+            margin="dense"
+            label='Password'
+            variant='filled'
+            value={adminPassword}
+            onChange={(e) => setAdminPassword(e.target.value)}
+          />
+          <Button variant='contained' onClick={handleAdminLogin}>
+            Login
+          </Button>
+        </ThemeProvider>
       </AdminLoginWrapper>
       }
     </AdminRoot>
