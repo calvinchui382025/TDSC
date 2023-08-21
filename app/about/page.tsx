7b15671b73190af53058dcb84ac7b08d0367516e
Lucas Overbey <lucasaoverbey@gmail.com>
2023-08-20 19:10:40 -0500

    fixed bios component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import styled from "@emotion/styled"
-import Separator from "app/Components/Separator";
+import Separator, { CustomParallax, ParallaxContainer, ParallaxContent, ParallaxTitle } from "app/Components/Separator";
 import { AboutButton, AboutImage, AboutRoot, BiosCard, BiosGrid, StyledDescription, StyledName, StyledTitle, WhatWeDoTextContainer, WhoWeAreContainer, WhoWeAreTextContainer } from "./AboutStyles";
 import { Button } from "@mui/material";
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
@@ -15,30 +15,51 @@ const whoWeAreImage = 'https://github.com/snyperifle/TDSC/blob/lukebranch/app/Co
 const whatWeDoImage = 'https://github.com/snyperifle/TDSC/blob/lukebranch/app/Components/assets/A7R05695.jpg?raw=true'
 const separateBanner = 'https://github.com/snyperifle/TDSC/blob/lukebranch/app/Components/assets/biosedit.jpg?raw=true'
 
+const BiosParallaxContainer = styled(ParallaxContainer)({
+  height: '85vh',
+  '@media (max-width: 668px)': {
+    height: '120vh',
+  },
+})
+
+const BiosCustomParallax = styled(CustomParallax)({
+  height: '85vh',
+  '@media (max-width: 668px)': {
+    height: '120vh',
+  },
+})
+
 const LeaderBiosComponent = () => {
   return (
-    <BiosGrid>
-      {peopleList.map((person) => (
-        <BiosCard key={person.name}>
-          {/* <img src={person.picture}style={{height: '20vh', borderRadius: '10px'}}/> */}
-          <StyledName>{person.name}</StyledName>
-          <StyledTitle>{person.title}</StyledTitle>
-          <StyledDescription>{person.subtitle}</StyledDescription>
-          <StyledDescription>{person.description}</StyledDescription>
-          <StyledDescription>{person.descriptiontwo}</StyledDescription>
-          <StyledDescription>{person.descriptionthree}</StyledDescription>
-          <StyledDescription>{person.descriptionfour}</StyledDescription>
-          {/* <Button
-            style={{
-              color: 'rgb(102, 164, 255)',
-              fontWeight: 'bold',
-            }}
-          >
-            Contact <KeyboardArrowRightIcon style={{backgroundImage: 'linear-gradient(to right, rgb(37, 83, 185), rgb(102, 164, 255)', borderRadius: '50%', color: greyColorCustomLight, marginLeft: 7}}/>
-          </Button> */}
-        </BiosCard>
-      ))}
-    </BiosGrid>
+    <BiosCustomParallax
+    bgImage={separateBanner}
+    strength={950}
+    >
+      <BiosParallaxContainer>
+        <ParallaxTitle>Club leader bios</ParallaxTitle>
+        <BiosGrid>
+          {peopleList.map((person) => (
+            <BiosCard key={person.name}>
+              <StyledName>{person.name}</StyledName>
+              <StyledTitle>{person.title}</StyledTitle>
+              <StyledDescription>{person.subtitle}</StyledDescription>
+              <StyledDescription>{person.description}</StyledDescription>
+              <StyledDescription>{person.descriptiontwo}</StyledDescription>
+              <StyledDescription>{person.descriptionthree}</StyledDescription>
+              <StyledDescription>{person.descriptionfour}</StyledDescription>
+              {/* <Button
+                style={{
+                  color: 'rgb(102, 164, 255)',
+                  fontWeight: 'bold',
+                }}
+              >
+                Contact <KeyboardArrowRightIcon style={{backgroundImage: 'linear-gradient(to right, rgb(37, 83, 185), rgb(102, 164, 255)', borderRadius: '50%', color: greyColorCustomLight, marginLeft: 7}}/>
+              </Button> */}
+            </BiosCard>
+          ))}
+        </BiosGrid>
+      </BiosParallaxContainer>
+    </BiosCustomParallax>
   )
 }
 
@@ -80,13 +101,13 @@ const peopleList = [
   },
 ]
 
-const pageSeparatorData = {
-  title: 'Club leader bios',
-  // content: 'Meet our ',
-  image: separateBanner,
-  separatorheight: '85vh',
-  dom: <LeaderBiosComponent />,
-}
+// const pageSeparatorData = {
+//   title: 'Club leader bios',
+//   // content: 'Meet our ',
+//   image: separateBanner,
+//   separatorheight: '85vh',
+//   dom: <LeaderBiosComponent />,
+// }
 
 const TempContainer = styled('div')({
   position: 'relative',
@@ -132,18 +153,6 @@ const TempContentTwo = styled('div')({
   padding: 20,
   boxSizing: 'border-box',
   color: 'white',
-  // display: 'flex',
-  // flexDirection: 'column',
-  // justifyContent: 'center',
-  // alignItems: 'center',
-  // width: '100%',
-  // height: '100%',
-  // backgroundImage: `linear-gradient(0deg, rgba(37, 27, 74, 0) 42%, ${mainColor} 93%)`,
-  // backgroundPosition: `center top, center bottom`,
-  // backgroundRepeat: 'no-repeat',
-  // padding: 20,
-  // boxSizing: 'border-box',
-  // color: 'white',
 });
 
 const TempH1 = styled('h1')({
@@ -204,45 +213,9 @@ export default function AboutPage() {
         </TempContentTwo>
       </TempContainerTwo>
       <div style={{width: '100%', height: '10vh', backgroundColor: mainColor}} />
-      <Separator data={pageSeparatorData}/>
+      <LeaderBiosComponent />
       <div style={{width: '100%', height: '10vh', backgroundColor: mainColor}} />
       <Contact />
     </AboutRoot>
   )
 }
-
-
-
-
-      {/* <Reveal>
-        <WhoWeAreContainer>
-          <AboutImage src={whoWeAreImage}/>
-          <WhoWeAreTextContainer>
-            <h1 style={{color: 'white', fontFamily: 'sans-serif'}}>Who we are</h1>
-            <h3 style={{color: 'white', fontFamily: 'sans-serif', display: 'inline-block'}}>
-              We are Texas Defensive Shooting Club (TDSC)- Our mission is to lead shooters to become well
-              acquainted with their weapon systems, advance speed and accuracy proficiency and promote
-              gun safety in order to increase the chance of survival in a deadly threat encounter.
-            </h3>
-            <AboutButton>
-              Join now! <KeyboardArrowRightIcon />
-            </AboutButton>
-          </WhoWeAreTextContainer>
-        </WhoWeAreContainer>
-      </Reveal>
-      <Reveal>
-        <WhoWeAreContainer>
-          {isMobile && <AboutImage src={whatWeDoImage} />}
-          <WhatWeDoTextContainer>
-            <h1 style={{color: 'white', fontFamily: 'sans-serif'}}>What we do</h1>
-            <h3 style={{color: 'white', fontFamily: 'sans-serif', textAlign: 'right'}}>
-              TDSC trains/leads defensive and tactical drills combined with practical scenarios running pistols
-              and carbines using various targets and props.
-            </h3>
-            <AboutButton>
-              Join now! <KeyboardArrowRightIcon />
-            </AboutButton>
-          </WhatWeDoTextContainer>
-          {!isMobile && <AboutImage src={whatWeDoImage} />}
-        </WhoWeAreContainer>
-      </Reveal> */}
